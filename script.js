299b80181015a05400bbf72894eb8a4e483025c8
Filippo Valle <filippo.valle@edu.unito.it>
2017-09-03 18:18:51 +0200

    abandon jekyll

---
+++ script.js
@@ -0,0 +1,17 @@
+function contacted() {
+  document.getElementById('telegram').style.color="#AAAAAA";
+  document.getElementById('mail').style.color="#AAAAAA";
+  document.getElementById('contact-thanks').innerHTML="Thank you for your message!";
+}
+
+function getAppVersion(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var version=JSON.parse(this.responseText);
+            document.getElementById("app-version").innerHTML ="my app is now at version "+version["version"];
+       }
+    };
+  xhttp.open("GET","http://phil-personal-api.herokuapp.com/Temperature_version.php?key=xenoncursedavitindiesyb",true);
+  xhttp.send();
+}
