8d3bd652bbb93fc75157c67297de072fa1a2fee6
Filippo Valle <filippo.valle@edu.unito.it>
2022-05-24 11:42:34 +0200

    update js message

--- js/hire.js
+++ js/hire.js
@@ -1,4 +1,4 @@
 function body_welcome(){
-    console.log("***** Hey, what are you doing here?*****\n");
-    console.log("***** Wanna hire me -> me@fvalle.online*****\n");
+    console.log("***** Hey, what are you doing here? *****\n");
+    console.log("***** Do you want to hire me -> me@fvalle.online *****\n");
 }
